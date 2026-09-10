6742044b953eb37851027b32c29baa073a990dac
Kenny <kritiraj.tech@gmail.com>
2026-09-11 01:46:20 +0530

    Fix icon swap end-of-animation size snap

--- src/components/icon-swap.tsx
+++ src/components/icon-swap.tsx
@@ -15,14 +15,10 @@ export function IconSwapItem({
 }) {
   return (
     <Component
-      initial={{ opacity: 0, scale: 0.25, filter: "blur(4px)" }}
-      animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
-      exit={{ opacity: 0, scale: 0.25, filter: "blur(4px)" }}
-      transition={{
-        type: "spring",
-        duration: 0.3,
-        bounce: 0,
-      }}
+      initial={{ opacity: 0, scale: 0.25 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.25 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
       {...props}
     />
   )
